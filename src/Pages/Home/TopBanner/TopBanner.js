046bf721d964fff6e175c6fec30a73730321967e
salikur6 <salikur6@gmail.com>
2022-05-12 19:47:15 +0600

    top banner section done

--- src/Pages/Home/TopBanner/TopBanner.js
+++ src/Pages/Home/TopBanner/TopBanner.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import chair1 from '../../../assets/images/chair.png';
+import bg from '../../../assets/images/bg.png';
 
 const TopBanner = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" alt='Top banner img' />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <div className="hero min-h-screen px-12" style={{ backgroundImage: `url(${bg})` }}>
+                <div className="hero-content flex-col lg:flex-row-reverse gap-5">
+                    <img src={chair1} className="max-w-sm rounded-lg shadow-2xl" alt='Top banner img' />
+                    <div className='py-12'>
+                        <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-primary uppercase text-white font-bold  bg-gradient-to-r from-secondary to-primary">Get Started</button>
                     </div>
                 </div>
             </div>
