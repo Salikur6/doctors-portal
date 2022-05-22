5c4275097997682147d595cc54b0382539d12883
salikur6 <salikur6@gmail.com>
2022-05-22 19:51:36 +0600

    usetoken api change

--- src/Hooks/useToken.js
+++ src/Hooks/useToken.js
@@ -8,7 +8,7 @@ const useToken = (user) => {
 
         const currentUser = { email: email };
         if (email) {
-            fetch(`http://localhost:5000/user/${email}`, {
+            fetch(`https://ancient-ravine-10764.herokuapp.com/user/${email}`, {
                 method: 'PUT',
                 headers: {
                     "Content-Type": "application/json"
